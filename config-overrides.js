bbe2c8d2bf4b4712e1991d70b42b3a44742cebc2
zhangjie <1278447561@qq.com>
2019-07-31 10:01:29 +0800

    config

---
+++ config-overrides.js
@@ -0,0 +1,29 @@
+const { override, fixBabelImports, addLessLoader,addWebpackAlias} = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+    fixBabelImports('import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true,
+           }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { 
+            "@primary-color": "#105CFB",
+            "@linerBackground":"linear-gradient(90deg,#40a9ff 0,#105cfb 100%)",
+        },
+    }),
+    addWebpackAlias({
+        '@': path.resolve(__dirname, 'src'), //配置全局路径
+    }),
+    (config) =>{
+        config.module.rules[2].oneOf[5].use.push({
+            loader: require.resolve('sass-resources-loader'),
+            options: {
+              resources:path.resolve(__dirname, 'src/assets/styles/common.less')
+            }
+        })
+        return config
+      }
+);
---
+++ src/components/pc/Header.js
@@ -0,0 +1,14 @@
+import React,{ Component } from "react";
+
+class Heaer extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        <div>
+            这个是头部组件
+        </div>
+    }
+}
+
+ export default Heaer;
