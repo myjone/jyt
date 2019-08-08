83d7d58f4063d68f80e42e5f8117670b6b18f3a4
zhangjie <1278447561@qq.com>
2019-08-08 20:08:13 +0800

    init

--- src/pages/pc/module/Home.js
+++ src/pages/pc/module/Home.js
@@ -25,6 +25,11 @@ import yingxiaotuiguang from "@/assets/images/yingxiaotuiguang.jpeg"
 import yunyingzhidao from "@/assets/images/yunyingzhidao.jpeg"
 import shichangyingxiao from "@/assets/images/shichangyingxiao.jpeg"
 import Logo from "@/assets/images/logo.png"
+import pyunyingzhidao from "@/assets/images/pyunyingzhidao.jpeg"
+import tangdaxiao from "@/assets/images/tangdaxiao.jpeg"
+import tianshengguiyiner from "@/assets/images/tianshengguiyiner.jpeg"
+import chizhiliang from "@/assets/images/chizhiliang.jpeg"
+import leyixiang from "@/assets/images/leyixiang.jpeg"
 const { Link } = Anchor;
 class Home extends Component {
     constructor() {
@@ -67,6 +72,26 @@ class Home extends Component {
                     info: '翰飞科技通过对超过40个行业和场景的深度了解探索及数据收集为商家提供更多更适合的运营管理经验，从自助开店，营销工具应用，内部培训，分销渠道开辟，系统化管理，数据互通，简化运营等方面为客户提供专业解答，线上视频教学，同行社交活动，运营官特训等无微不至的体贴服务！'
                 }
             ],
+            caseList:[
+                {
+                    title:'天生贵银耳',
+                    info:'成都天生贵商贸有限公司成立于2017年，该公司主要经营四川通江的段木银耳；起初天生贵在四川开设有多家的实体门店，基于互联网的迅速发展，现在天生贵在线上搭建有多家微商城小店，通过线下的流量引流，为实体店铺节省了更多的成本。',
+                    src:tianshengguiyiner
+                },
+                {
+                    title:'车之靓名车维修美容',
+                    info:'车之靓美容公司为周边的小区户主开设社区便捷服务，可快速维修、上门服务等业务；通过微商城的服务优势为户主提供线上预约、查看车辆信息、商家维修进度，同时车主也可在微商城查看近期活动、会员套餐等。车之靓致力于服务中国广大车主。',
+                    src:chizhiliang
+                },{
+                    title:'乐益享',
+                    info:'翰飞科技微商城为乐益享超市提供了专属的订单系统，乐益享的顾客能像网购一样订货；解决了店铺业务员电话沟通、上门抄单等不便问题，其中微商城还提供了丰富的营销玩法、例如商品打折、优惠券、红包等等，有效解决生意难的困扰',
+                    src:leyixiang
+                },{
+                    title:'唐大侠龙虾',
+                    info:'唐大侠龙虾店铺开设在商圈周边，每天成交量约在120单左右，为吸引、照顾更多的周边顾客，唐大侠龙虾店开通了微商城的搭建，通过微商城的平台，为商圈周围的顾客提供了便捷的点餐服务，客户只需在线上点餐，商家可通过上门服务的方式，为顾客提供更便捷、周到的服务',
+                    src:tangdaxiao
+                }
+            ]
         }
     }
 
@@ -95,8 +120,7 @@ class Home extends Component {
         this.map.addControl(navigationControl);
     }
     render() {
-        const { serviceList } = this.state;
-        console.log(serviceList)
+        const { serviceList , caseList} = this.state;
         return (
             <div>
                 <div className='nav whiteNav'>
@@ -146,7 +170,7 @@ class Home extends Component {
 
                     </Carousel>
                 </div>
-                <div className='w1200' id='products'>
+                <div className='w1200 pdt80' id='products'>
                     <ul className='advantages'>
                         {
                             this.state.list.map((item, index) => {
@@ -189,7 +213,7 @@ class Home extends Component {
                         </h1>
                         <Row>
                             <Col lg={{ span: 12 }} xs={{ span: 12 }}>
-                            <p className='textFontSize16 textLineHeight50'>
+                            <p className='textFontSize16 textLineHeight50 pdr73'>
                                 有效解决零基础上手难的门槛，微商城内置丰富的营销工具让你轻松卖货，经典的分销玩法，让你的成交额快速提升。
                             </p>
                             </Col>
@@ -209,7 +233,7 @@ class Home extends Component {
                     <Row>
                         <Col lg={{ span: 12 }} xs={{ span: 12 }}>
                             <div className="himage">
-
+                                <img src={pyunyingzhidao}></img>
                             </div>
                         </Col>
                         <Col lg={{ span: 12 }} xs={{ span: 12 }}>
@@ -365,89 +389,26 @@ class Home extends Component {
                         他们正在使用聚易推
                     </h1>
                     <Row>
-                        <Col span={6}>
-                            <div className='box boxShowdow hoverBoxScale' style={{ width: "280px" }}>
-                                <div className='mask homeMask p20'>
-                                    <div className="img mgb18">
 
+                        {
+                            caseList.map((item,index)=>{
+                                return(
+                                    <Col span={6} key={index+item.src}>
+                                    <div className='box boxShowdow hoverBoxScale radius5 bgCommon' style={{ width: "280px",backgroundImage:`url(${item.src})` }}>
+                                        <div className='mask homeMask p20'>
+                                            <div className="img pdt20 mgb18 textCenter colorWhite textFontSize20">
+                                               {item.title}
+                                            </div>
+                                            <p className='textCnter colorWhite'>
+                                                {item.info}
+                                            </p>
+                                        </div>
                                     </div>
-                                    <p className="textCenter colorWhite">
-                                        "这个是标题" 某一个酒家
-                                    </p>
-                                    <p className='textCnter colorWhite'>
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                    </p>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col span={6}>
-                            <div className='box boxShowdow hoverBoxScale' style={{ width: "280px" }}>
-                                <div className='mask homeMask p20'>
-                                    <div className="img mgb18">
-
-                                    </div>
-                                    <p className="textCenter colorWhite">
-                                        "这个是标题" 某一个酒家
-                                    </p>
-                                    <p className='textCnter colorWhite'>
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                    </p>
-                                </div>
-
-                            </div>
-                        </Col>
-                        <Col span={6}>
-                            <div className='box boxShowdow hoverBoxScale' style={{ width: "280px" }}>
-                                <div className='mask homeMask p20'>
-                                    <div className="img mgb18">
-
-                                    </div>
-                                    <p className="textCenter colorWhite">
-                                        "这个是标题" 某一个酒家
-                                    </p>
-                                    <p className='textCnter colorWhite'>
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                    </p>
-                                </div>
-                            </div>
-                            <p className='homeUsers textFontSize16 textLeft textColor666' >
-                                双11活动大促期间，每日新增会员数超过<span className='textColorPrimary'>999</span>
-                            </p>
-                        </Col>
-                        <Col span={6}>
-                            <div className='box boxShowdow hoverBoxScale' style={{ width: "280px" }}>
-                                <div className='mask homeMask p20'>
-                                    <div className="img mgb18">
-
-                                    </div>
-                                    <p className="textCenter colorWhite">
-                                        "这个是标题" 某一个酒家
-                                    </p>
-                                    <p className='textCnter colorWhite'>
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                        描述 描述 描述 描述 描述 描述 描述 描述 描述 描述
-                                    </p>
-                                </div>
-                            </div>
-                            <p className='homeUsers textFontSize16 textLeft textColor666' >
-                                双11活动大促期间，每日新增会员数超过<span className='textColorPrimary'>999</span>
-                            </p>
-                        </Col>
+                                </Col>
+                                )
+                            })
+                        }
+                      
                     </Row>
                 </div>
                 <div className='wFull  pdt80 pdb30' id='about'>
